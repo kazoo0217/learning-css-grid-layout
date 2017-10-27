c34f68e0f805255ddf52618fdc908245fad68f81
Kazuhito Tsujita <kazuhito.tsujita@gmail.com>
2017-10-27 17:14:30 +0900

    environment

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({//ベンダープレフィックス付けて
+      browsers: ['last 2 major versions', 'ie >= 10'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./html/css/'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/scss/*.scss', ['sass']);
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./html"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('default', ['browser-sync','sass:watch'], function () {
+  gulp.watch("./html/*.html", ['bs-reload']);
+  gulp.watch("./html/css/*.css", ['bs-reload']);
+});
